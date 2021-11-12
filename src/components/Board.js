34076c933169b20da7d788671f493b3b78896f1e
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-11-12 11:12:35 +0100

    initial commit

---
+++ src/components/Board.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Square } from 'components/Square'
+
+export const Board = () => {
+  const squares = useSelector((store) => store.game.squares)
+
+  return (
+    <div className="board">
+      {squares.map((value, index) => (
+        <Square key={index} value={value} index={index} />
+      ))}
+    </div>
+  )
+}
---
+++ src/components/Square.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { game } from 'reducers/game'
+import { Player } from 'components/Player'
+
+export const Square = ({ value, index }) => {
+  const dispatch = useDispatch()
+  const handleClick = () => {
+    dispatch(game.actions.captureSquare({ index }))
+  }
+
+  return (
+    <button
+      className={value === null ? 'square clickable' : 'square'}
+      type="button"
+      onClick={handleClick}>
+      <Player value={value} />
+    </button>
+  )
+}
